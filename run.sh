3fc9e1754b53cd63994b0cff6aaa0e64d1b754f5
mkbeh <mkbehforever@gmail.com>
2018-03-18 12:32:02 -0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,12 @@
 #!/usr/bin/env bash
 
+sudo git clone https://github.com/smartholdem/smartholdem-rpc.git
+cd smartholdem-rpc
+npm install
+forever start server.js
+
 sudo apt install python-pip
-cd withdrawal-cons
+cd withdrawal-coins
 pip install -r requirements.txt
-python withdrawal_coins.py
+sudo python withdrawal_coins.py
+forever stop server.js
